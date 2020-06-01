919d07d166ad79da627fa5701609e4cd5df136bb
Loren Lisk <loren.lisk@foghornconsulting.com>
2020-06-01 14:48:50 -0400

    updated pull script.

--- download.sh
+++ download.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p ./rootfs
-curl -skLo ./rootfs/alpine-minirootfs-3.10.3-armhf.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/armhf/alpine-minirootfs-3.10.3-armhf.tar.gz
-curl -skLo ./rootfs/alpine-minirootfs-3.10.3-x86_64.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/x86_64/alpine-minirootfs-3.10.3-x86_64.tar.gz
-
+curl -skLo ./rootfs/alpine-minirootfs-3.11.3-armhf.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/armhf/alpine-minirootfs-3.11.3-armhf.tar.gz
+curl -skLo ./rootfs/alpine-minirootfs-3.11.3-x86_64.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.3-x86_64.tar.gz

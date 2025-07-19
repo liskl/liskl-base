ac97e3728de86c859adeaeef076ef96bbda22a09
Loren Lisk <loren.lisk@liskl.com>
2025-07-19 18:41:03 -0400

    feat: comprehensive Alpine Linux base image overhaul
    - Update to Alpine 3.22.1 as latest version
    - Add support for Alpine versions 3.14.3 through 3.22.1
    - Implement multi-platform builds (amd64, arm/v7)
    - Add matrix strategy for building all Alpine versions
    - Update Dockerfile for TARGETARCH support
    - Modernize GitHub Actions workflows with buildx
    - Add comprehensive README documentation
    - Download script now pulls all supported Alpine versions
    - Use semantic versioning for image tags (alpine-X.Y.Z)
    - Feature branches tagged with commit SHA for uniqueness

--- download.sh
+++ download.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
 mkdir -p ./rootfs
-curl -skLo ./rootfs/alpine-minirootfs-3.11.3-armhf.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/armhf/alpine-minirootfs-3.11.3-armhf.tar.gz
-curl -skLo ./rootfs/alpine-minirootfs-3.11.3-x86_64.tar.gz http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.3-x86_64.tar.gz
+
+# Alpine Linux minirootfs downloads for versions 3.14.3 through 3.22.1
+versions=(
+    "3.14.3"
+    "3.15.11" 
+    "3.16.9"
+    "3.17.10"
+    "3.18.12"
+    "3.19.8"
+    "3.20.7"
+    "3.21.4"
+    "3.22.1"
+)
+
+for version in "${versions[@]}"; do
+    major_minor=$(echo $version | cut -d. -f1,2)
+    echo "Downloading Alpine $version..."
+    curl -skLo "./rootfs/alpine-minirootfs-${version}-armhf.tar.gz" "https://dl-cdn.alpinelinux.org/alpine/v${major_minor}/releases/armhf/alpine-minirootfs-${version}-armhf.tar.gz"
+    curl -skLo "./rootfs/alpine-minirootfs-${version}-x86_64.tar.gz" "https://dl-cdn.alpinelinux.org/alpine/v${major_minor}/releases/x86_64/alpine-minirootfs-${version}-x86_64.tar.gz"
+done
